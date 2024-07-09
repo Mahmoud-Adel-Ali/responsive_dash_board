a14f4acd23537335f10d4f42763c7c4d35338b5b
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-07-09 14:58:55 -0700

    add detailed income chart

---
+++ lib/widgets/detailed_income_chart.dart
@@ -0,0 +1,63 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_styless.dart';
+
+class DetailedIncomeChart extends StatefulWidget {
+  const DetailedIncomeChart({super.key});
+
+  @override
+  State<DetailedIncomeChart> createState() => _DetailedIncomeChartState();
+}
+
+class _DetailedIncomeChartState extends State<DetailedIncomeChart> {
+  int currentIndex = -1;
+  @override
+  Widget build(BuildContext context) {
+    var pieChartData = PieChartData(
+      sectionsSpace: 0,
+      pieTouchData: PieTouchData(
+        touchCallback: (p0, p1) {
+          currentIndex = p1?.touchedSection?.touchedSectionIndex ?? -1;
+          setState(() {});
+        },
+      ),
+      sections: [
+        PieChartSectionData(
+          titlePositionPercentageOffset: currentIndex == 0 ? 1.5 : null,
+          title: currentIndex == 0 ? 'Design service' : '40%',
+          titleStyle: AppStyless.styleSemiBold18
+              .copyWith(color: currentIndex == 0 ? const Color(0xff208CC8) : null),
+          color: const Color(0xff208CC8),
+          value: 40,
+          radius: currentIndex == 0 ? 45: 40,
+          // showTitle: false,
+        ),
+        PieChartSectionData(
+          color: const Color(0xff4EB7F2),titlePositionPercentageOffset: currentIndex == 1 ? -1.5 : null,
+          title: currentIndex == 1 ? 'Design product' : '25%',
+          titleStyle: AppStyless.styleSemiBold18
+              .copyWith(color: currentIndex == 1 ? const Color(0xff208CC8) : null),
+          value: 25,
+          radius: currentIndex == 1 ? 45: 40,
+        ),
+        PieChartSectionData(
+          color: const Color(0xff064061),titlePositionPercentageOffset: currentIndex == 2 ? 1.5 : null,
+          title: currentIndex == 2 ? 'Product royalti' : '20%',
+          titleStyle: AppStyless.styleSemiBold18
+              .copyWith(color: currentIndex == 2 ? const Color(0xff208CC8) : null),
+          value: 20,
+          radius: currentIndex == 2 ? 45: 40,
+        ),
+        PieChartSectionData(
+          color: const Color(0xffE2DECD),titlePositionPercentageOffset: currentIndex == 3 ? -1.2 : null,
+          title: currentIndex == 3 ? 'Other' : '22%',
+          titleStyle: AppStyless.styleSemiBold18
+              .copyWith(color: currentIndex == 3 ? const Color(0xff208CC8) : null),
+          value: 22,
+          radius: currentIndex == 3 ? 45: 40,
+        ),
+      ],
+    );
+    return AspectRatio(aspectRatio: 1, child: PieChart(pieChartData));
+  }
+}
--- lib/widgets/income_body.dart
+++ lib/widgets/income_body.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/detailed_income_chart.dart';
 import 'package:responsive_dash_board/widgets/income_chart.dart';
 import 'package:responsive_dash_board/widgets/income_item_list_tile.dart';
 
@@ -9,38 +12,45 @@ class IncomeBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      children: [
-        Expanded(flex: 3, child: IncomeChart()),
-        // SizedBox(width: 10),
-        Expanded(
-          flex: 5,
-          child: Column(
+    log(MediaQuery.sizeOf(context).width.toString());
+    final double width = (MediaQuery.sizeOf(context).width);
+    return width < 1270 && width > 1200
+        ? const Row(children: [Expanded( child: Padding(
+          padding: EdgeInsets.symmetric( horizontal:  16.0),
+          child: DetailedIncomeChart(),
+        ))])
+        : const Row(
             children: [
-              IncomeItemListTile(
-                title: 'Design service',
-                value: 40,
-                leadingColor: Color(0xff208CC8),
-              ),
-              IncomeItemListTile(
-                title: 'Design product',
-                value: 25,
-                leadingColor: Color(0xff4EB7F2),
-              ),
-              IncomeItemListTile(
-                title: 'Product royalti',
-                value: 20,
-                leadingColor: Color(0xff064061),
-              ),
-              IncomeItemListTile(
-                title: 'Other',
-                value: 22,
-                leadingColor: Color(0xffE2DECD),
+              Expanded(flex: 2, child: IncomeChart()),
+              // SizedBox(width: 10),
+              Expanded(
+                flex: 5,
+                child: Column(
+                  children: [
+                    IncomeItemListTile(
+                      title: 'Design service',
+                      value: 40,
+                      leadingColor: Color(0xff208CC8),
+                    ),
+                    IncomeItemListTile(
+                      title: 'Design product',
+                      value: 25,
+                      leadingColor: Color(0xff4EB7F2),
+                    ),
+                    IncomeItemListTile(
+                      title: 'Product royalti',
+                      value: 20,
+                      leadingColor: Color(0xff064061),
+                    ),
+                    IncomeItemListTile(
+                      title: 'Other',
+                      value: 22,
+                      leadingColor: Color(0xffE2DECD),
+                    ),
+                  ],
+                ),
               ),
             ],
-          ),
-        ),
-      ],
-    );
+          );
   }
 }
